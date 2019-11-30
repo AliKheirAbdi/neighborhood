001b670d877bca46156b74e0294a1acc9d9ef1da
AliKheir <akheirali@gmail.com>
2019-11-30 17:04:01 +0300

    setup project files

--- neighbor/views.py
+++ neighbor/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def registration(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            return redirect('/login')
+    else:
+        form = RegisterForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'users/register.html', context)
