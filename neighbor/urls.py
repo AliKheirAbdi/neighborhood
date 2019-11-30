25800613ea1e1046b554dd44fec0a860720e0758
AliKheir <akheirali@gmail.com>
2019-11-30 17:33:04 +0300

    added user profile path to urlpatterns

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('search/', views.search_business, name="search"),
+    path('profile/', views.profile, name='profile'),
 
 ]
 if settings.DEBUG:
