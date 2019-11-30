9f6ab2eec195c95bdf1416fd1099b216cc8f64af
AliKheir <akheirali@gmail.com>
2019-12-01 00:15:29 +0300

    added StoryForm class to forms.py

--- neighbor/forms.py
+++ neighbor/forms.py
@@ -21,3 +21,9 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['name', 'neighborhood']
+
+class StoryForm(forms.ModelForm):
+
+    class Meta:
+        model= Post
+        fields= ['title', 'story', 'neighborhood']
