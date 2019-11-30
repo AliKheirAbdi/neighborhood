97a6ca5944826bdbd1d5b573066266b7d71758ee
AliKheir <akheirali@gmail.com>
2019-11-30 17:30:41 +0300

    added login path to urlpatterns

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('home',views.home,name='home'),
     path('register/', views.registration, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
 
 ]
 if settings.DEBUG:
